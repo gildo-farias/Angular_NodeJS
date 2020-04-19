344c0d016e8c293d9382d656c58c1f0fe7b268ec
Gildo Farias <57909027+gildo-farias@users.noreply.github.com>
2020-04-19 01:53:44 -0300

    REST API
    Iniciado a parte de rest, criando db.json para persistencia dos dados da aplicação.

--- src/services/livros.service.ts
+++ src/services/livros.service.ts
@@ -108,6 +108,10 @@ export class LivrosService {
 
   getLivros(){        
     return [this.l1, this.l2, this.l3, this.l4, this.l5, this.l6, this.l7, this.l8];  
+    // let livros:any=[];
+    // this._http.get<Livro[]>('assets/data/livros.json').subscribe(data => {      
+    // });
+    
   }
 
   getLivro(cod: number){        
